685778eeb3c917319e3c1fb12c218775a24c2c82
setare askari <setareaskari79@gmail.com>
2021-11-09 15:24:04 +0100

    install bootstrap

--- src/App.js
+++ src/App.js
@@ -1,48 +1,46 @@
-import logo from './logo.svg';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.min.js';
-import './App.css';
-import MyBody from './Site/homepage'
-import { BrowserRouter as Router } from 'react-router-dom';
-import {Routes, Route, Link, Navigate} from 'react-router-dom'
-import MyNavbar from './Site/navbar';
-import { Container } from 'react-bootstrap';
-import LoginPage from './Site/loginpage';
-import { LoginForm } from './Site/login';
+import logo from "./logo.svg";
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import "./App.css";
+import MyBody from "./Site/homepage";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Routes, Route, Link, Navigate } from "react-router-dom";
+import MyNavbar from "./Site/navbar";
+import { Container } from "react-bootstrap";
+import LoginPage from "./Site/loginpage";
+import { LoginForm } from "./Site/login";
 function App() {
   return (
-  <Router>
-      <MyNavbar/>
+    <Router>
+      <MyNavbar />
       <Container fluid className="below-nav vh-100">
-      <Routes>
-        <Route path="/" element={<Navigate replace to="/home" />} />          
-              
+        <Routes>
+          <Route path="/" element={<Navigate replace to="/home" />} />
+
           {/* Generic Error Page */}
-          <Route path='/error' element = {<ErrorPage/>}/>
-          
-            {/*Route di Login*/}
-          <Route path="/loginpage" element = {<LoginPage/>}/> 
-          
-          <Route path ="/loginpage/:type" element = {<LoginForm/>} />
-            {/*Route di Registrazione*/}
-          <Route path="/sign-in" /*element = {</>}*/
-          />
+          <Route path="/error" element={<ErrorPage />} />
+
+          {/*Route di Login*/}
+          <Route path="/loginpage" element={<LoginPage />} />
+
+          <Route path="/loginpage/:type" element={<LoginForm />} />
+          {/*Route di Registrazione*/}
+          <Route path="/sign-in" /*element = {</>}*/ />
           {/* BODY PER HOMEPAGE */}
-          <Route exact path="/home" element = {<MyBody/>}/>
-        
-      </Routes>
+          <Route exact path="/home" element={<MyBody />} />
+        </Routes>
       </Container>
     </Router>
-    
   );
 }
 function ErrorPage() {
   return (
-    <Container fluid className='center vh-100 below-nav'>
+    <Container fluid className="center vh-100 below-nav">
       <h1>Page not found</h1>
-      <Link to='/'>Go Back to Home</Link>
+      <Link to="/">Go Back to Home</Link>
     </Container>
-  )
+  );
 }
 
 export default App;
