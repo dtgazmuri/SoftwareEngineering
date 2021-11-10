caa329adec67778fce125dd3c1b1ece40455eb08
stestrippoli <s282870@studenti.polito.it>
2021-11-10 19:16:47 +0100

    bug fixes

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
@@ -94,59 +93,15 @@ function App() {
       </Container>
     </Router>
     
-=======
-import logo from "./logo.svg";
-import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.min.js";
-import "./App.css";
-import MyBody from "./Site/homepage";
-import { BrowserRouter as Router } from "react-router-dom";
-import { Routes, Route, Link, Navigate } from "react-router-dom";
-import MyNavbar from "./Site/navbar";
-import { Container } from "react-bootstrap";
-import LoginPage from "./Site/loginpage";
-import { LoginForm } from "./Site/login";
-function App() {
-  return (
-    <Router>
-      <MyNavbar />
-      <Container fluid className="below-nav vh-100">
-        <Routes>
-          <Route path="/" element={<Navigate replace to="/home" />} />
-
-          {/* Generic Error Page */}
-          <Route path="/error" element={<ErrorPage />} />
-
-          {/*Route di Login*/}
-          <Route path="/loginpage" element={<LoginPage />} />
-
-          <Route path="/loginpage/:type" element={<LoginForm />} />
-          {/*Route di Registrazione*/}
-          <Route path="/sign-in" /*element = {</>}*/ />
-          {/* BODY PER HOMEPAGE */}
-          <Route exact path="/home" element={<MyBody />} />
-        </Routes>
-      </Container>
-    </Router>
->>>>>>> c23fd517c2f19990aed79fb0f037c9ae3f25cb8e
   );
 }
 function ErrorPage() {
   return (
-<<<<<<< HEAD
     <Container fluid className='center vh-100 below-nav'>
       <h1>Page not found</h1>
       <Link to='/'>Go Back to Home</Link>
     </Container>
   )
-=======
-    <Container fluid className="center vh-100 below-nav">
-      <h1>Page not found</h1>
-      <Link to="/">Go Back to Home</Link>
-    </Container>
-  );
->>>>>>> c23fd517c2f19990aed79fb0f037c9ae3f25cb8e
 }
 
 export default App;
