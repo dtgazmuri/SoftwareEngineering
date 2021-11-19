f1535e62fa033341764237fdba01b8b1c388a84a
LorenzoMolteni <lorenzomolteni2@gmail.com>
2021-11-19 11:06:39 +0100

    text-decoration for create a client

--- client/src/Site/Employee/shopemployeepage.js
+++ client/src/Site/Employee/shopemployeepage.js
@@ -16,7 +16,7 @@ function MyPage(props) {
       <Container fluid className="below-nav vh-100 align-items-center">
         <Row id="first">
           <Col id="signin-col" >
-            <Container fluid className="LoginButton border border-dark rounded nolink" align="center" onClick={handleShow}>
+            <Container fluid className="LoginButton border border-dark rounded nolink text-decoration" align="center" onClick={handleShow}>
               {signin}
               <h3>Create new Client</h3>
             </Container>
