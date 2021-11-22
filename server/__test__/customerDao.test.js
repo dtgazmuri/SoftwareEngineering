39d0873c07f370668d406b3d9da23df50f0d611f
setare askari <setareaskari79@gmail.com>
2021-11-22 16:03:42 +0100

    test customerDao

---
+++ server/__test__/customerDao.test.js
@@ -0,0 +1,66 @@
+const customerDao = require("../Dao/customerDao");
+const db = require("../db");
+
+const initializeDB = () => {
+  return new Promise((resolve, reject) => {
+    const sql = "DELETE FROM customer;";
+    db.run(sql, [], (err) => {
+      if (err) {
+        reject(err);
+        return;
+      } else resolve();
+    });
+  });
+};
+
+const addCustomerForTest = (customer) => {
+  return new Promise((resolve, reject) => {
+    const sql = "INSERT INTO customer(NAME, SURNAME, WALLET) VALUES (?, ? , ?)";
+    db.run(
+      sql,
+      [customer.NAME, customer.SURNAME, customer.WALLET],
+      function (err) {
+        if (err) {
+          reject(err);
+          return;
+        }
+        resolve(this.lastID);
+      }
+    );
+  });
+};
+describe("Test customerDao functions", () => {
+  beforeAll(() => {
+    initializeDB();
+  });
+
+  // afterAll(async () => {});
+  afterAll(() => {
+    // initializeDB();
+  });
+  test("test getCustomerByUserId when id does not exist", async () => {
+    return expect(customerDao.getCustomerByUserId(1)).rejects.toEqual({
+      code: "404",
+      msg: "not found",
+    });
+  });
+  test("test getCustomerByUserId when id exists", async () => {
+    const newCustomer = { NAME: "setare", SURNAME: "askari", WALLET: 100 };
+    addCustomerForTest(newCustomer)
+      .then((id) => {
+        return customerDao.getCustomerByUserId(id).then((data) => {
+          expect(data).toEqual([
+            {
+              ID: id,
+              NAME: newCustomer.NAME,
+              SURNAME: newCustomer.SURNAME,
+              WALLET: newCustomer.WALLET,
+            },
+          ]);
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+});
