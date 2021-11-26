a000110d598629a3b570d6a3536d533ad7b72032
setare askari <setareaskari79@gmail.com>
2021-11-26 21:59:29 +0100

    farmerDao test done

--- server/__test__/farmerDao.test.js
+++ server/__test__/farmerDao.test.js
@@ -3,7 +3,8 @@ const db = require("../dbTest");
 
 const initializeDB = () => {
   return new Promise((resolve, reject) => {
-    const sql = "DELETE FROM product; DELETE FROM farmer;";
+    const sql =
+      "DELETE FROM product; DELETE FROM farmer; DELETE FROM warehouse;";
     db.run(sql, [], (err) => {
       if (err) {
         reject(err);
@@ -39,7 +40,7 @@ const addProductForTest = (product, farmerId) => {
   });
 };
 
-beforeAll(async () => {
+beforeEach(async () => {
   await initializeDB();
 });
 
@@ -56,20 +57,33 @@ describe("test farmerDao functions", () => {
 
   test("test getFarmerProducts", async () => {
     addFarmerForTest({ NAME: "test", SURNAME: "test" }).then((farmerId) => {
-      const product = {
+      const newProduct = {
         NAME: "apple",
         PRICE: 100,
       };
-      addProductForTest(product, farmerId).then((productId) => {
+      addProductForTest(newProduct, farmerId).then((productId) => {
         return farmerDao.getFarmerProducts(db, farmerId).then((data) => {
           expect(data).toEqual([
             {
               id: productId,
-              name: product.NAME,
-              price: product.PRICE,
+              name: newProduct.NAME,
+              price: newProduct.PRICE,
             },
           ]);
         });
+      });
+    });
+  });
+
+  test("test ADDProductExpectedAmount", async () => {
+    const product = {
+      product: 3,
+      quantity: 14,
+    };
+    return farmerDao.addProductExpectedAmount(db, product).then((data) => {
+      expect(data).toEqual({
+        product: product.product,
+        quantity: product.quantity,
       });
     });
   });
